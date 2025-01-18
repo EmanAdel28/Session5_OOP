b4225004ffc59ff2ee2cc404812a43f362fe6038
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-18 19:03:05 +0200

    First Project

---
+++ Assignment5_OOP/FirstProject/Point3D.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5_OOP.FirstProject
+{
+    internal class Point3D : IComparable , ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point3D(int x , int y , int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Point3D(int x, int y) : this(x , y , 0) 
+        {
+            
+            
+        }
+
+        public Point3D(int x) : this(x, 0, 0)
+        {
+           
+
+        }
+        public Point3D() : this(0, 0, 0) 
+        { 
+        }
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Point3D P =(Point3D) obj;
+
+
+           
+            if (this.X > P.X) return 1;
+            if (this.X < P.X) return -1;
+
+           
+            if (this.Y > P.Y) return 1;
+            if (this.Y < P.Y) return -1;
+
+            
+            if (this.Z > P.Z) return 1;
+            if (this.Z < P.Z) return -1;
+
+            
+            return 0;
+        }
+
+        public object Clone()
+        {
+            return new Point3D()
+            {
+                X = this.X,
+                Y = this.Y,
+                Z = this.Z
+            };
+        }
+
+        public static bool operator ==(Point3D left, Point3D right)
+        {
+            if (left.X == right.X)
+            {
+                if (left.Y == right.Y)
+                {
+                    return (left.Z == right.Z);
+                }
+            }
+            return false;
+
+        }
+
+        public static bool operator !=(Point3D left, Point3D right)
+        {
+            if (left.X == right.X)
+            {
+                if (left.Y == right.Y)
+                {
+                    return (left.Z == right.Z);
+                }
+            }
+            return false;
+
+        }
+
+
+    }
+}
--- Assignment5_OOP/Program.cs
+++ Assignment5_OOP/Program.cs
@@ -1,10 +1,99 @@
-﻿namespace Assignment5_OOP
+﻿using System.Drawing;
+using Assignment5_OOP.FirstProject;
+
+namespace Assignment5_OOP
 {
     internal class Program
     {
+        static int ReadCoordinate(String CoordinateName)
+        {
+            Console.WriteLine($"Enter : {CoordinateName} ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int result))
+            {
+                return result;
+            }
+
+            try
+            {
+                return int.Parse(input);
+            }
+            catch
+            {
+                return Convert.ToInt32(input);
+            }
+        }
+
+        static Point3D ReadPoint()
+        {
+            int X = ReadCoordinate("x");
+            int Y = ReadCoordinate("y");
+            int Z = ReadCoordinate("z");
+
+            return new Point3D(X, Y, Z);
+        }
+       
         static void Main(string[] args)
         {
-          
+            #region First Project
+            //1. Define 3D Point Class and the basic Constructors (use chaining in constructors).
+            //2. Override the ToString Function to produce this output:
+            //Point3D P = new Point3D(10, 10, 10); 
+            //Console.WriteLine(P.ToString());
+            //Output: “Point Coordinates: (10, 10, 10)”.
+
+            //3. Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+            //4. Try to use == If(P1 == P2)   Does it work properly? 
+            //5. Define an array of points and sort this array based on X & Y coordinates.
+            //6. Implement ICloneable interface to be able to clone the object To implement more than one interface.
+            //class Point3D : IComparable, ICloneable
+
+            //Point3D P = new Point3D(10);
+            //Console.WriteLine(P.ToString());
+
+
+            //Point3D P1 = ReadPoint();
+            //Point3D P2 = ReadPoint();
+
+            //Console.WriteLine(P1.ToString());
+            //Console.WriteLine(P2.ToString());
+
+
+            //if (P1 == P2)  // Invalid i want to make  operator overload 
+            //{
+            //    Console.WriteLine("P1 == P2")
+            // }
+
+            // After Overloading Operator
+            //if (P1 == P2)
+            //{
+            //    Console.WriteLine("P1 == P2");
+            // }
+            //else
+            //{
+            //    Console.WriteLine("P1 Not Equal P2");
+            //}
+
+            //      Point3D[] points = new Point3D[]
+            //{
+            //      new Point3D(3, 5, 2),
+            //      new Point3D(1, 2, 8),
+            //      new Point3D(3, 2, 4),
+            //      new Point3D(2, 3, 7),
+            //      new Point3D(0, 2, 6)
+            //};
+            //      Array.Sort(points);
+            //      foreach(Point3D p in points)
+            //          { Console.WriteLine(p.ToString()); }
+
+            //Point3D P1 = new Point3D(1, 4, 3);
+            //Point3D P2 = null;
+
+            //P2 = (Point3D)P1.Clone();
+            //Console.WriteLine(P2);
+            
+            #endregion
         }
     }
 }
