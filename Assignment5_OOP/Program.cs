ec7cfd92f0ea8a2d806bb3439f58bf33367aa6a1
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-18 21:05:50 +0200

    Third Project

--- Assignment5_OOP/Program.cs
+++ Assignment5_OOP/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing;
 using Assignment5_OOP.FirstProject;
 using Assignment5_OOP.SecondProject;
+using Assignment5_OOP.ThirdProject;
 
 namespace Assignment5_OOP
 {
@@ -124,7 +125,72 @@ namespace Assignment5_OOP
             //    Console.WriteLine(e.Message );
             //}
 
-            
+
+            #endregion
+
+            #region Third Project
+
+            // 1.Define Class Duration To include Three Attributes Hours, Minutes and Seconds.
+            // 2.Override All System.Object Members (ToString, Equals,GetHasCode) .
+            // 3.Define All Required Constructors to Produce this output: 
+            //Duration D1 = new Duration(1, 10, 15);
+            //D1.ToString();
+            //Output: Hours: 1, Minutes: 10, Seconds: 15
+
+            //Duration D1 = new Duration(3600);
+            //D1.ToString();
+            //Output: Hours: 1, Minutes: 0, Seconds: 0
+
+            //Duration D2 = new Duration(7800);
+            //D2.ToString();
+            //Output: Hours: 2, Minutes: 10, Seconds: 0
+
+            //Duration D3 = new Duration(666);
+            //D3.ToString();
+            //Output: Minutes: 11, Seconds: 6
+
+            // 4.Implement All required Operators overloading to enable this Code:
+            //D3 = D1 + D2
+            //D3 = D1 + 7800
+            //D3 = 666 + D3
+            //D3 = ++D1(Increase One Minute)
+            //D3 = --D2(Decrease One Minute)
+            //D1 = D1 - D2
+            //If(D1 > D2)
+            //If(D1 <= D2)
+            //If(D1)
+            //DateTime Obj = (DateTime)D1
+
+            //Duration D1 = new Duration(1, 10, 15);
+            //Console.WriteLine(D1.ToString()); 
+
+            //Duration D2 = new Duration(3600);
+            //Console.WriteLine(D2.ToString()); 
+
+            //Duration D3 = new Duration(7800);
+            //Console.WriteLine(D3.ToString()); 
+
+            //Duration D4 = new Duration(666);
+            //Console.WriteLine(D4.ToString());
+
+
+            Duration D1 = new Duration(1, 10, 15);
+            Duration D2 = new Duration(2, 20, 19);
+            Duration D3 = null;
+
+            D3 = D1 + D2;
+            D3 = D1 + 7800;
+            D3 = 666 + D3;
+            D3 = D1++;
+            D3 = --D2;
+            D1 = D2 - D1;
+
+            if(D1)
+            {
+
+            }
+            DateTime Obj = (DateTime)D1;
+            Console.WriteLine(Obj.ToString());
             #endregion
         }
     }
